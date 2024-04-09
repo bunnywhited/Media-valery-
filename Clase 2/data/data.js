baf71b4610103854f23558a7ee9d0887d044cc38
Valery <hoyosvalentina1407@gmail.com>
2024-04-09 09:47:59 -0500

    ejercicio terminado

--- Clase 2/data/data.js
+++ Clase 2/data/data.js
@@ -2,7 +2,28 @@ export const pinturas = [
 
     {
         id:1,
-        name :"",
+        name :"Dancer at the bar",
+        Image:"./imagenes/Dancer at the bar.jpg"
+    },
+    {
+        id:1,
+        name :"Guitarra",
+        Image:"./imagenes/guitarra.jpg"
+    },
+    {
+        id:1,
+        name :"La muerte de Pablo Escobar",
+        Image:"./imagenes/La muerte de plablo escobar.jpg"
+    },
+    {
+        id:1,
+        name :"Monalisa a los 12 años",
+        Image:"./imagenes/monalisa a los 12 años.jpg"
+    },
+    {
+        id:1,
+        name :"Mujer fumando",
         Image:"./imagenes/mujer fumando.jpg"
-    }
+    },
+    
 ]
--- Clase 2/index.js
+++ Clase 2/index.js
@@ -1,3 +1,25 @@
-import { pinturas} from "./data/data";
+import { pinturas} from "./data/data.js";
 
 console.log(pinturas)
+
+const items=document.getElementById('items')
+
+const templateCard= document.getElementById ('template-card').content
+
+const fragment =document.createDocumentFragment()
+
+const LoadData = pinturas => {
+ pinturas.forEach(data => { 
+
+    const{ id,name,Image } = data
+    templateCard.querySelector('h5').textContent = name
+    templateCard.querySelector('img').setAttribute('src',Image)
+    const clone = templateCard.cloneNode(true)
+    fragment.appendChild(clone)
+});
+items.appendChild(fragment)
+}
+
+document.addEventListener('DOMContentLoaded',()=> {
+    LoadData(pinturas)
+})
